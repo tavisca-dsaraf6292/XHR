39d4440de2b5e529d8ec74b60fb42bc7d1765759
dsaraf@tavisca.com <dsaraf@tavisca.com>
2014-07-29 14:59:46 +0530

    final commit for Hotels and Car

--- xhr.js
+++ xhr.js
@@ -33,7 +33,6 @@ function loadXMLDoc()
 					para.id="p"+i;
 					var element=document.getElementById('paragraph');
 					element.appendChild(para);
-					//document.body.appendChild(para);
 					showImage(obj.deals[i].imageUrl,100,100,"No Image Available");
 					document.getElementById(para.id).innerHTML = "<hr/>" + "Hotel Name:" + " " + obj.deals[i].hotelName + "<br>" + " " + "Hotel Id:" + " " + obj.deals[i].hotelId + "<br>" + "Title:" + obj.deals[i].title +"<br>" + "Hotel Description:" + " " + obj.deals[i].description + "<br>" + " " +"Booking Period:"+"<br>" + "&nbsp;&nbsp;&nbsp&nbsp    Start:" + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp&nbsp;&nbsp;&nbsp&nbsp    Date:" + obj.deals[i].bookingPeriod.start.date +"<br>"+"&nbsp;&nbsp;&nbsp&nbsp&nbsp;&nbsp;&nbsp&nbsp Time:" + obj.deals[i].bookingPeriod.start.time+"<br>" +"&nbsp;&nbsp;&nbsp&nbsp&nbsp;&nbsp;&nbsp&nbsp System Date Time:" + obj.deals[i].bookingPeriod.start.systemDateTime+"<br>" +"&nbsp;&nbsp;&nbsp&nbsp    End:" + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp&nbsp;&nbsp;&nbsp&nbsp    Date:" + obj.deals[i].bookingPeriod.end.date +"<br>"+"&nbsp;&nbsp;&nbsp&nbsp&nbsp;&nbsp;&nbsp&nbsp Time:" + obj.deals[i].bookingPeriod.end.time+"<br>" +"&nbsp;&nbsp;&nbsp&nbsp&nbsp;&nbsp;&nbsp&nbsp System Date Time:" + obj.deals[i].bookingPeriod.end.systemDateTime+"<br>" + "Fare:" +"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Total Amount:" + obj.deals[i].fare.totalAmount+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Currency Code:" + obj.deals[i].fare.currencyCode+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Currency Symbol:" + obj.deals[i].fare.currencySymbol +"<br>"+ "Location:" +"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp City:" +obj.deals[i].location.city + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp Code:" +obj.deals[i].location.code+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp State:" +obj.deals[i].location.state + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp Country:" + obj.deals[i].location.country;
 					i++;
@@ -49,7 +48,6 @@ function loadXMLDoc()
 			img.alt=alt;
 			var element=document.getElementById('paragraph');
 			element.appendChild(img);
-			//document.body.appendChild(img);
 		}
 		xmlhttp.open("GET","http://dev-mystique.tavisca.com/api/deals/all?token=0fchwyho1hw2uwhfr3dsjq21&$filter=Type eq 'hotel'",true);
 		xmlhttp.send();
@@ -75,7 +73,7 @@ function loadXMLDoc()
 					var element=document.getElementById('paragraph');
 					element.appendChild(para);
 					//document.body.appendChild(para);
-					document.getElementById(para.id).innerHTML = "Rental Company Name:" + " " + obj.deals[i].rentalCompanyName + "<br>" + " " + "Rental Company Code:" + " " + obj.deals[i].rentalCompanyCode + "<br>" + "Title:" + obj.deals[i].title +"<br>" + "Booking Period:" + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp    Date:" + obj.deals[i].bookingPeriod.start.date +"<br>"+"&nbsp;&nbsp;&nbsp&nbsp Time:" + obj.deals[i].bookingPeriod.start.time+"<br>" + "Fare:" +"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Total Amount:" + obj.deals[i].fare.totalAmount+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Currency Code:" + obj.deals[i].fare.currencyCode+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Currency Symbol:" + obj.deals[i].fare.currencySymbol +"<br>"+ "Location:" +"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp City:" +obj.deals[i].location.city + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp Code:" +obj.deals[i].location.code+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp State:" +obj.deals[i].location.state + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp Country:" +obj.deals[i].location.country;
+					document.getElementById(para.id).innerHTML = "<hr/>" + "Rental Company Name:" + " " + obj.deals[i].rentalCompanyName + "<br>" + " " + "Rental Company Code:" + " " + obj.deals[i].rentalCompanyCode + "<br>" + "Title:" + obj.deals[i].title +"<br>" + "Booking Period:" + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp    Date:" + obj.deals[i].bookingPeriod.start.date +"<br>"+"&nbsp;&nbsp;&nbsp&nbsp Time:" + obj.deals[i].bookingPeriod.start.time+"<br>" + "Fare:" +"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Total Amount:" + obj.deals[i].fare.totalAmount+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Currency Code:" + obj.deals[i].fare.currencyCode+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp Currency Symbol:" + obj.deals[i].fare.currencySymbol +"<br>"+ "Location:" +"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp City:" +obj.deals[i].location.city + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp Code:" +obj.deals[i].location.code+"<br>"+ "&nbsp;&nbsp;&nbsp&nbsp State:" +obj.deals[i].location.state + "<br>" + "&nbsp;&nbsp;&nbsp&nbsp Country:" +obj.deals[i].location.country;
 					i++;
 
 				}
@@ -85,6 +83,3 @@ function loadXMLDoc()
 		xmlhttp.send();
 	}
 }
-	/*function refreshPage(){
-    window.location.reload();
-} */
